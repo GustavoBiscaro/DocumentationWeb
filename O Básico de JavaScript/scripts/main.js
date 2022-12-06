f74caca8f51710cb19ca238058da11a0ad7e1bca
Gustavo Miguel <gustavo.biscaro@gmail.com>
2022-12-06 16:55:42 -0300

    first commit

---
+++ O Básico de JavaScript/scripts/main.js
@@ -0,0 +1,14 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Olá mundo!";
+
+
+let myVariable = "Bob";
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+multiply(4, 7);
+multiply(20, 20);
+multiply(0.5,3);
