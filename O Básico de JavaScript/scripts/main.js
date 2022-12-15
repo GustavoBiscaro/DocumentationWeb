780707c87aff1063a437de13b29468ec7df89192
Gustavo Miguel <gustavo.biscaro@gmail.com>
2022-12-15 16:46:49 -0300

    .

--- O Básico de JavaScript/scripts/main.js
+++ O Básico de JavaScript/scripts/main.js
@@ -1,40 +1,41 @@
 const myImage = document.querySelector("img");
 const myHeading = document.querySelector("h1");
 const myButton = document.querySelector("button");
+const myBack = document.querySelector("background");
 
 function trocarImagens() {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "img/firefox.webp") {
-        myImage.setAttribute("src", "img/chrome.png");
-    } else {
-        myImage.setAttribute("src", "img/firefox.webp");
-    }
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "img/firefox.webp") {
+    myImage.setAttribute("src", "img/chrome.png");
+  } else {
+    myImage.setAttribute("src", "img/firefox.webp");
+  }
 }
 
 function trocarTexto() {
-    const mySrc = myImage.getAttribute("src");
-    if(mySrc == "img/firefox.webp"){
-        myHeading.textContent = "Mozilla is cool";
-    } else {
-        myHeading.textContent = "Mas eu prefiro o Chrome!";
-    }
+  const minhaPesquisa = myImage.getAttribute("src");
+  if (minhaPesquisa == "img/firefox.webp") {
+    myHeading.textContent = "Mozilla is cool";
+  } else {
+    myHeading.textContent = "Mas eu prefiro o Chrome!";
+  }
 }
 
 function setUserName() {
-    const myName = prompt("Your name please!");
-    if (!myName) {
-        setUserName();
-    } else{
+  const myName = prompt("Your name please!");
+  if (!myName) {
+    setUserName();
+  } else {
     localStorage.setItem("name", myName);
     myHeading.textContent = `Mozilla is cool, ${myName}`;
-    }
+  }
 }
 
 myButton.onclick = () => {
-    setUserName(); 
+  setUserName();
 };
 
 myImage.onclick = () => {
-    trocarImagens();
-    trocarTexto();
+  trocarImagens();
+  trocarTexto();
 };
